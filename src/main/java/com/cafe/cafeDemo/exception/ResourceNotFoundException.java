4360164e83d23ec7626b2290344d1601af084c60
oemsalas <oemsalas@Vostro-3500>
2026-03-29 20:42:25 -0300

    se borraron los recursos

--- src/main/java/com/cafe/cafeDemo/exception/ResourceNotFoundException.java
+++ src/main/java/com/cafe/cafeDemo/exception/ResourceNotFoundException.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+    private static final long serialVersionUID = -5986003061545677254L;
+
+	public ResourceNotFoundException(String message) {
         super(message);
     }
 }
